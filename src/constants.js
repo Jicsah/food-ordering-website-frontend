23f9662d956f721053894b61649dd70968e31aff
Jicsah <jicsahsaju@gmail.com>
2021-09-06 15:09:14 +0530

    modified production URL

--- src/constants.js
+++ src/constants.js
@@ -1,4 +1,4 @@
 const DEVELOPMENT_URL = "http://localhost:5454";
 const PRODUCTION_URL = "https://floating-lowlands-66739.herokuapp.com/";
-//
+//production url updated
 exports.API_URL = PRODUCTION_URL; 
